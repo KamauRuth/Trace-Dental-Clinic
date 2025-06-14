ea84ebd4b8fb97ec4904292c35c15cb3c9d91c1f
KamauRuth <rkamau573@gmail.com>
2025-06-15 00:23:06 +0300

    trace dental

--- Backend/server.js
+++ Backend/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const path = require('path');
 
 require('dotenv').config();
 
@@ -22,23 +23,31 @@ const corsOptions = {
 app.use(cors(corsOptions));
 
 // Middleware setup
-app.use(express.json());  // To parse JSON bodies
+app.use(express.json());
+  // To parse JSON bodies
 
 const PORT = process.env.PORT || 5000;
 
 
 
-
-
-
-// Middleware
-// app.use(cors()); // Removed duplicate CORS middleware
-app.use(express.json());
-
-
 // Routes
 app.use('/api/appointment', router);
 app.use('/api/callback', callRouter);
+
+app.use(express.urlencoded({ extended: true }));
+
+// Serve static files from the Frontend folder
+app.use(express.static(path.join(__dirname, "../Frontend")));
+
+// Serve index.html for "/"
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "../Frontend/index.html"));
+});
+
+// Serve appointment.html for "/appointment"
+app.get("/appointment", (req, res) => {
+  res.sendFile(path.join(__dirname, "../Frontend/appointment.html"));
+});
 
 // Start server
 app.listen(PORT, () => {
